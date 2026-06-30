e7e015534ab794a5377712ed268e58c51a1cb9bb
johanbartholdy120999-debug <johanbartholdy120999@gmail.com>
2026-06-30 22:37:45 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -621,6 +621,9 @@ def main():
 
 if __name__ == "__main__":
     main()
-  from plot_models import create_all_plots
+ from plot_models import create_all_plots
 
-create_all_plots()
+
+if __name__ == "__main__":
+    main()
+    create_all_plots()
